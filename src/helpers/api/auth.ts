e0f954c0c7c22957ede2585ffe38603c8e0789ef
mbkhole@gmail.com <mbkhole@gmail.com>
2025-07-21 10:08:24 +0530

    removing all Iframes

--- src/helpers/api/auth.ts
+++ src/helpers/api/auth.ts
@@ -1,26 +1,27 @@
 import { APICore } from './apiCore';
 
 const api = new APICore();
+const BASE_URL = process.env.REACT_APP_API_BASE_URL as string;
 
 // account
 function login(params: { email: string; password: string }) {
-    const baseUrl = 'http://localhost:5000/api/auth/login';
-    return api.create(baseUrl, params);
+    const url = `${BASE_URL}/auth/login`;
+    return api.create(url, params);
 }
 
 function logout() {
-    const baseUrl = 'http://localhost:5000/api/auth/logout';
-    return api.create(baseUrl, {});
+    const url = `${BASE_URL}/auth/logout`;
+    return api.create(url, {});
 }
 
 function signup(params: { fullname: string; email: string; password: string }) {
-    const baseUrl = 'http://localhost:5000/api/auth/register';
-    return api.create(baseUrl, params);
+    const url = `${BASE_URL}/auth/register`;
+    return api.create(url, params);
 }
 
 function forgotPassword(params: { email: string }) {
-    const baseUrl = 'http://localhost:5000/api/auth/forget-password';
-    return api.create(baseUrl, params);
+    const url = `${BASE_URL}/auth/forget-password`;
+    return api.create(url, params);
 }
 
 export { login, logout, signup, forgotPassword };
--- src/pages/ImpactWorkReports/data.ts
+++ src/pages/ImpactWorkReports/data.ts
@@ -31,42 +31,42 @@ const post3: Post[] = [
         tag: { variant: 'success', value: 'Annual Reports' },
         title: 'Annual Report 2022-23',
     },
-    {
-        link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2022/01/VOPA-Annual-report-2020-21_compressed-1.pdf&embedded=true',
-        docUrl: 'https://vopa.in/wp-content/uploads/2022/01/VOPA-Annual-report-2020-21_compressed-1.pdf',
-        tag: { variant: 'success', value: 'Annual Reports' },
-        title: 'Annual Report 2020-21',
-    },
-    {
-        link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/Report-2018-19-Complete_compressed.pdf&embedded=true',
-        docUrl: 'https://vopa.in/wp-content/uploads/2021/04/Report-2018-19-Complete_compressed.pdf',
-        tag: { variant: 'success', value: 'Annual Reports' },
-        title: 'Annual Report 2018-19',
-    },
-    {
-        link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_Work_Report_April-June_2019_English_compressed.pdf&embedded=true',
-        docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_Work_Report_April-June_2019_English_compressed.pdf',
-        tag: { variant: 'success', value: 'Quarterly Reports' },
-        title: 'April-June_2019',
-    },
-    {
-        link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q6_Oct_Dec_19_Eng3_compressed.pdf&embedded=true',
-        docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q6_Oct_Dec_19_Eng3_compressed.pdf',
-        tag: { variant: 'success', value: 'Quarterly Reports' },
-        title: 'October-December_2019',
-    },
-    {
-        link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q5_July_Sept_eng_2019_compressed.pdf&embedded=true',
-        docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q5_July_Sept_eng_2019_compressed.pdf',
-        tag: { variant: 'success', value: 'Quarterly Reports' },
-        title: 'Jully-September_2019',
-    },
-    {
-        link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_Quarterly_Work_Report_Jan_Mar_2020_English_compressed.pdf&embedded=true',
-        docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_Quarterly_Work_Report_Jan_Mar_2020_English_compressed.pdf',
-        tag: { variant: 'success', value: 'Quarterly Reports' },
-        title: 'Jan-March_2020',
-    },
+    // {
+    //     link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2022/01/VOPA-Annual-report-2020-21_compressed-1.pdf&embedded=true',
+    //     docUrl: 'https://vopa.in/wp-content/uploads/2022/01/VOPA-Annual-report-2020-21_compressed-1.pdf',
+    //     tag: { variant: 'success', value: 'Annual Reports' },
+    //     title: 'Annual Report 2020-21',
+    // },
+    // {
+    //     link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/Report-2018-19-Complete_compressed.pdf&embedded=true',
+    //     docUrl: 'https://vopa.in/wp-content/uploads/2021/04/Report-2018-19-Complete_compressed.pdf',
+    //     tag: { variant: 'success', value: 'Annual Reports' },
+    //     title: 'Annual Report 2018-19',
+    // },
+    // {
+    //     link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_Work_Report_April-June_2019_English_compressed.pdf&embedded=true',
+    //     docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_Work_Report_April-June_2019_English_compressed.pdf',
+    //     tag: { variant: 'success', value: 'Quarterly Reports' },
+    //     title: 'April-June_2019',
+    // },
+    // {
+    //     link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q6_Oct_Dec_19_Eng3_compressed.pdf&embedded=true',
+    //     docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q6_Oct_Dec_19_Eng3_compressed.pdf',
+    //     tag: { variant: 'success', value: 'Quarterly Reports' },
+    //     title: 'October-December_2019',
+    // },
+    // {
+    //     link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q5_July_Sept_eng_2019_compressed.pdf&embedded=true',
+    //     docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_work_Q5_July_Sept_eng_2019_compressed.pdf',
+    //     tag: { variant: 'success', value: 'Quarterly Reports' },
+    //     title: 'Jully-September_2019',
+    // },
+    // {
+    //     link: 'https://docs.google.com/gview?url=https://vopa.in/wp-content/uploads/2021/04/VOPA_Quarterly_Work_Report_Jan_Mar_2020_English_compressed.pdf&embedded=true',
+    //     docUrl: 'https://vopa.in/wp-content/uploads/2021/04/VOPA_Quarterly_Work_Report_Jan_Mar_2020_English_compressed.pdf',
+    //     tag: { variant: 'success', value: 'Quarterly Reports' },
+    //     title: 'Jan-March_2020',
+    // },
 ];
 
 export { post3 };
